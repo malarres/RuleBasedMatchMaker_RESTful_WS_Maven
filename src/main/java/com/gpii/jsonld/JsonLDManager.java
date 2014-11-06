34a06c8b61c9ef365d75f28e51fdb9345579ffcf
Nick Kaklanis <nkak@iti.gr>
2014-11-06 12:06:28 +0200

    bug in integration test fixed
    - bug in integration test fixed
    - Utils.java was added

--- src/main/java/com/gpii/jsonld/JsonLDManager.java
+++ src/main/java/com/gpii/jsonld/JsonLDManager.java
@@ -12,9 +12,9 @@ import java.io.*;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+//import java.nio.charset.StandardCharsets;
+//import java.nio.file.Files;
+//import java.nio.file.Paths;
 import java.util.Iterator;
 import java.util.List;
 import org.json.JSONArray;
@@ -134,38 +134,7 @@ public class JsonLDManager
     }
 
     
-    /**
-     * TODO create a class for help functions
-     * @param path where to write the file
-     * @param dataToWrite // data to write in the file
-     */
-    private void writeFile(String path, byte[] dataToWrite){
-            FileOutputStream out = null;
-            try {
-                    out = new FileOutputStream(path);
-            } catch (FileNotFoundException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-            }
-            try {
-                    out.write(dataToWrite);
-                    out.close();
-            } catch (IOException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-            }
-    }
     
-    /**
-     * TODO create a class for help functions
-     * @param path where to write the file
-     * @param dataToWrite // data to write in the file
-     */
-	static String readFile(String path, Charset encoding) throws IOException 
-	{
-	  byte[] encoded = Files.readAllBytes(Paths.get(path));
-	  return new String(encoded, encoding);
-}
     
     
 }
--- src/test/java/com/gpii/restful/RBMM_WebServiceIT.java
+++ src/test/java/com/gpii/restful/RBMM_WebServiceIT.java
@@ -1,54 +1,45 @@
 package com.gpii.restful;
 
 
+import com.gpii.utils.Utils;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.charset.Charset;
 
 import static junit.framework.Assert.assertEquals;
 import junit.framework.TestCase;
+import sun.nio.cs.StandardCharsets;
 
 public class RBMM_WebServiceIT extends TestCase
 {
     public void testWS() 
     {
-        String inputFileStr = null;
-        String outputFileStr = null;
+        String inputJsonStr = null;
+        String expectedOutputJsonStr = null;
         
-        File fileIN = new File(System.getProperty("user.dir") + "/src/main/webapp/WEB-INF/testData/preferences/newMMInput.json");
-        File fileOUT = new File(System.getProperty("user.dir") + "/src/main/webapp/WEB-INF/testData/configurations/newMMOutput.json");
+        String filepathIN = System.getProperty("user.dir") + "/src/main/webapp/WEB-INF/testData/preferences/input_test1.json";
+        String filepathExpectedOUT = System.getProperty("user.dir") + "/src/main/webapp/WEB-INF/testData/expectedTestOutcomes/expected_output_test1.json";
         
-        // read expected input
+        // read input & expected output
         try {
-            FileReader reader = new FileReader(fileIN);
-            char[] chars = new char[(int) fileIN.length()];
-            reader.read(chars);
-            inputFileStr = new String(chars);
-            reader.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        
-        // read expected output
-        try {
-            FileReader reader = new FileReader(fileOUT);
-            char[] chars = new char[(int) fileOUT.length()];
-            reader.read(chars);
-            outputFileStr = new String(chars);
-            reader.close();
+            inputJsonStr = Utils.getInstance().readFile(filepathIN);
+            expectedOutputJsonStr = Utils.getInstance().readFile(filepathExpectedOUT);
         } catch (IOException e) {
             e.printStackTrace();
         }
         
         Client client = Client.create();
         WebResource webResource = client.resource("http://localhost:8080/RBMM/runJSONLDRules");
-        ClientResponse response = webResource.accept("application/json").type("application/json").post(ClientResponse.class, inputFileStr);
+        ClientResponse response = webResource.accept("application/json").type("application/json").post(ClientResponse.class, inputJsonStr);
         String output = response.getEntity(String.class);
-        System.out.println("Output from Server ....\n");
+        
+        System.out.println("\nOutput from Server...\n");
         System.out.println(output);
-        assertEquals(output, outputFileStr);
+               
+        assertEquals(output, expectedOutputJsonStr);
     }
 }
